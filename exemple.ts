4d3af3367977669dc6adc1371d99632c05669df6
Azerus <kevin.marous@gmail.com>
2017-09-12 10:20:33 +0200

    first class

---
+++ exemple.ts
@@ -0,0 +1,19 @@
+export class Compteur {
+
+    constructor(private compte:number=0){}
+
+    increment() {
+this.compte++;
+
+    }
+    decrement() {
+
+        this.compte--;
+
+    }
+    showCount():number {
+
+        return this.compte;
+
+    }
+}
